886b6503a626542224ae6e9fd74af662c9079eca
Jacob Cruz <27jacobcruz@gmail.com>
2026-06-15 12:45:35 -0400

    copy and pasted the problems ive done so far (6 and 7)

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
 const repeatString = function() {
+ if (num < 0)
+    {
+        return "ERROR";
+    }
 
+    else
+    {
+        let word = "";
+
+        for (let i = 0; i < 3; i++)
+        {
+            word += string;
+        }
+        return word;
+    }
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function() {
-
+    let word = "";
+    for (let i = string.length - 1; i >= 0; i--)
+    {
+        word += string[i]
+    }
+    return word;
 };
 
 // Do not edit below this line
